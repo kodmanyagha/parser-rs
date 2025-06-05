9c75050961e913855b9c34bc88ae77345d9ed9b6
Emir Bugra Koksalan <kodmanyagha@gmail.com>
2025-06-05 12:28:58 +0300

    pest library added

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
 pub mod token;
 
+use pest::{Parser, iterators::Pair};
+use pest_derive::*;
 use std::str::FromStr;
 
 use token::{Token, Tokenizer};
+
+#[derive(Parser)]
+#[grammar = "sums.pest"]
+pub struct SumsParser;
 
 #[derive(PartialEq, Clone, Debug)]
 pub enum Expr {
@@ -23,14 +29,46 @@ pub fn op(o: Oper, a: Expr, b: Expr) -> Expr {
     Expr::Op(o, Box::new(a), Box::new(b))
 }
 
+// Panics if r not an Operator
+pub fn rule_to_op(r: &Rule) -> Oper {
+    match r {
+        Rule::sub => Oper::Sub,
+        Rule::div => Oper::Div,
+        Rule::mul => Oper::Mul,
+        Rule::add => Oper::Add,
+        _ => unreachable!(),
+    }
+}
+
+pub fn pair_to_expr(p: Pair<Rule>) -> Expr {
+    let rule = p.as_rule();
+    if Rule::number == rule {
+        return Expr::Num(p.as_str().parse::<i64>().unwrap());
+    }
+
+    // iterate over the child rules
+    let mut inner = p.into_inner();
+
+    let left = pair_to_expr(inner.next().unwrap());
+    let right = inner.next().map(pair_to_expr);
+
+    match rule {
+        Rule::item => left,
+        Rule::brackets => Expr::Brackets(Box::new(left)),
+        Rule::div | Rule::mul | Rule::sub | Rule::add => match right {
+            Some(r) => op(rule_to_op(&rule), left, r),
+            None => left,
+        },
+        _ => unreachable!(),
+    }
+}
+
 impl FromStr for Expr {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let t = Tokenizer::new(s);
-        let (_tokenizer, expr) = sub(&t)?;
-
-        Ok(expr)
+        let mut sp = SumsParser::parse(Rule::sub, s).map_err(|e| e.to_string())?;
+        Ok(pair_to_expr(sp.next().unwrap()))
     }
 }
 
@@ -121,7 +159,15 @@ pub fn sub<'a>(t: &Tokenizer<'a>) -> ParseRes<'a, Expr> {
 
 #[cfg(test)]
 mod tests {
+    use pest::Parser;
+
     use super::*;
+
+    #[test]
+    pub fn test_pest_returns_something() {
+        let mut sp = SumsParser::parse(Rule::sub, "23+4").unwrap();
+        assert_eq!(sp.next().unwrap().as_rule(), Rule::sub);
+    }
 
     #[test]
     fn test_strs() {
